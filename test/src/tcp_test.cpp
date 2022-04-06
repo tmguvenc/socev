0127e6aacd5be7a023064e27b621c2f0f915bac2
Turan Murat Guvenc <murat.guvenc@esensi.com.tr>
2022-04-06 23:30:54 +0300

    created testing environment

---
+++ test/src/tcp_test.cpp
@@ -0,0 +1,24 @@
+extern "C" {
+  #include "tcp_context.h"
+}
+
+#include <gtest/gtest.h>
+
+TEST(tcp_context, create_destroy) {
+
+  tcp_context_params params = {
+    .port = 9000,
+    .max_client_count = 2,
+    .callback = [](const event_type ev, void *c_info, const void *in,
+                   const unsigned int len){}
+  };
+
+  auto ctx = socev_create_tcp_context(params);
+  ASSERT_NE(ctx, nullptr);
+  socev_destroy_tcp_context(ctx);
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
