775469341da4d0cbf54c709c0857d74d4e09de87
Turan Murat Guvenc <murat.guvenc@esensi.com.tr>
2022-04-08 14:54:09 +0300

    minor changes

--- test/src/tcp_test.cpp
+++ test/src/tcp_test.cpp
@@ -17,23 +17,6 @@ TEST(tcp_context, create_destroy) {
   socev_destroy_tcp_context(ctx);
 }
 
-TEST(tcp_context, service) {
-  tcp_context_params params = {
-    .port = 9001,
-    .max_client_count = 2,
-    .callback = [](const event_type ev, void *c_info, const void *in,
-                   const unsigned int len){}
-  };
-
-  auto ctx = socev_create_tcp_context(params);
-  ASSERT_NE(ctx, nullptr);
-
-
-
-
-  socev_destroy_tcp_context(ctx);
-}
-
 int main(int argc, char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
