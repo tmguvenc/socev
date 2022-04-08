58527a210b4c95ecac791e29797d4a5a3a412005
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2022-04-08 14:56:16 +0300

    Feature/scv 4 (#5)
    * created testing environment
    
    * working on tests
    
    * minor changes
    
    * removed unused files
    
    Co-authored-by: Turan Murat Guvenc <murat.guvenc@esensi.com.tr>

--- lib/include/tcp_context.h
+++ lib/include/tcp_context.h
@@ -12,23 +12,23 @@ typedef enum {
 } event_type;
 
 typedef struct {
-  unsigned short port;
-  unsigned int max_client_count;
+  uint16_t port;
+  uint32_t max_client_count;
   void (*callback)(const event_type ev, void *c_info, const void *in,
-                   const unsigned int len);
+                   const uint32_t len);
 } tcp_context_params;
 
 void *socev_create_tcp_context(tcp_context_params params);
 void socev_destroy_tcp_context(void *ctx);
 
 const char *socev_get_client_ip(void *c_info);
-unsigned short socev_get_client_port(void *c_info);
+uint16_t socev_get_client_port(void *c_info);
 
 void socev_callback_on_writable(void *c_info);
 int socev_service(void *ctx, int timeout_ms);
 
 void socev_set_timer(void *c_info, const uint64_t timeout_us);
 
-int socev_write(void *c_info, const void *data, unsigned int len);
+int socev_write(void *c_info, const void *data, uint32_t len);
 
 #endif // LIB_TCP_CONTEXT_H_
