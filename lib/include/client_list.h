4e1da769d419b0599b523a61e11d8ea5670927ac
Turan Murat Guvenc <tguvenc@dataremote.com>
2023-07-06 17:15:10 +0000

    removed bool

--- lib/include/client_list.h
+++ lib/include/client_list.h
@@ -1,7 +1,6 @@
 #ifndef LIB_CLIENT_LIST_H_
 #define LIB_CLIENT_LIST_H_
 
-#include <stdbool.h>
 #include <stdint.h>
 
 typedef enum { FD_REGULAR = 0, FD_TIMER, __MAX_FD_CNT } fd_type_t;
@@ -15,10 +14,10 @@ void* client_list_create(uint16_t cnt);
 void client_list_destroy(void* cl);
 uint16_t client_list_get_count(void* cl);
 uint16_t client_list_get_max_count(void* cl);
-bool client_list_is_full(void* cl);
-bool client_list_is_empty(void* cl);
-bool client_list_add_client(void* cl, void* ci);
-bool client_list_del_client(void* cl, int client_fd);
+int client_list_is_full(void* cl);
+int client_list_is_empty(void* cl);
+int client_list_add_client(void* cl, void* ci);
+int client_list_del_client(void* cl, int client_fd);
 int client_list_get_client(void* cl, int fd, client_get_result_t* out);
 
 #endif  // LIB_CLIENT_LIST_H_
